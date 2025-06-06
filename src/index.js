5c4063dc762876c1730d9025ef628124d3f1da7f
epiloguebooks <epiloguebooksnet@gmail.com>
2025-06-06 09:12:04 -0500

    index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./NavBar";
 import Home from "./Home";
+import Search from "./Search";
 import Quiz from "./Quiz";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -11,6 +12,7 @@ root.render(
     <NavBar />
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/search" element={<Search />} />
       <Route path="/quiz" element={<Quiz />} />
     </Routes>
   </BrowserRouter>
