7cd181ffa7896939b0169db698e79dd76ce7dec2
epiloguebooks <epiloguebooksnet@gmail.com>
2025-06-06 18:16:36 -0500

    App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Quiz from './Quiz';
+import QuizList from './QuizList';
+import NavBar from './NavBar';
 
 function App() {
   return (
     <Router>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/quiz" element={<Quiz />} />
+        <Route path="/quizzes" element={<QuizList />} />
       </Routes>
     </Router>
   );
