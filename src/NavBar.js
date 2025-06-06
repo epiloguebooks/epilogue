8e4f31ff7ac40c810bdad63af6217594691c45b2
epiloguebooks <epiloguebooksnet@gmail.com>
2025-06-06 09:15:36 -0500

    NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,20 +1,27 @@
-<nav
-  style={{
-    padding: "1rem 2rem",
-    backgroundColor: "#222",
-    color: "white",
-    display: "flex",
-    gap: "2rem",
-    fontFamily: "sans-serif",
-  }}
->
-  <Link to="/" style={{ color: "white", textDecoration: "none" }}>
-    Home
-  </Link>
-  <Link to="/search" style={{ color: "white", textDecoration: "none" }}>
-    Search
-  </Link>
-  <Link to="/quiz" style={{ color: "white", textDecoration: "none" }}>
-    Quizzes
-  </Link>
-</nav>
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function NavBar() {
+  return (
+    <nav
+      style={{
+        padding: "1rem 2rem",
+        backgroundColor: "#222",
+        color: "white",
+        display: "flex",
+        gap: "2rem",
+        fontFamily: "sans-serif",
+      }}
+    >
+      <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+        Home
+      </Link>
+      <Link to="/search" style={{ color: "white", textDecoration: "none" }}>
+        Search
+      </Link>
+      <Link to="/quiz" style={{ color: "white", textDecoration: "none" }}>
+        Quizzes
+      </Link>
+    </nav>
+  );
+}
