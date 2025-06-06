f4621dc431d4e8ea6ab728421bd2a6e3ccdb288f
epiloguebooks <epiloguebooksnet@gmail.com>
2025-06-06 08:35:05 -0500

    index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Quiz from "./Quiz";
+import Home from "./Home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Quiz />);
+root.render(<Home />);
+
