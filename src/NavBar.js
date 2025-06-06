ae72fd4b15d38ccf2a9aac54849a28e3882415f0
epiloguebooks <epiloguebooksnet@gmail.com>
2025-06-06 09:12:25 -0500

    NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,25 +1,20 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
-export default function NavBar() {
-  return (
-    <nav
-      style={{
-        padding: "1rem 2rem",
-        backgroundColor: "#222",
-        color: "white",
-        display: "flex",
-        gap: "2rem",
-        fontFamily: "sans-serif",
-      }}
-    >
-      <Link to="/" style={{ color: "white", textDecoration: "none" }}>
-        Home
-      </Link>
-      <Link to="/quiz" style={{ color: "white", textDecoration: "none" }}>
-        Quizzes
-      </Link>
-      {/* You can add more tabs here later */}
-    </nav>
-  );
-}
+<nav
+  style={{
+    padding: "1rem 2rem",
+    backgroundColor: "#222",
+    color: "white",
+    display: "flex",
+    gap: "2rem",
+    fontFamily: "sans-serif",
+  }}
+>
+  <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+    Home
+  </Link>
+  <Link to="/search" style={{ color: "white", textDecoration: "none" }}>
+    Search
+  </Link>
+  <Link to="/quiz" style={{ color: "white", textDecoration: "none" }}>
+    Quizzes
+  </Link>
+</nav>
