1a607d91d6711583c54ca08cbeef910a7c497531
epiloguebooks <epiloguebooksnet@gmail.com>
2025-06-06 09:03:56 -0500

    index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./NavBar";
 import Home from "./Home";
 import Quiz from "./Quiz";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
+    <NavBar />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/quiz" element={<Quiz />} />
