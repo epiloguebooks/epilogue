8c1c9e173bcb28242f4207c7c7624dbb3aae6996
epiloguebooks <epiloguebooksnet@gmail.com>
2025-06-06 23:12:08 -0500

    Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,13 +2,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "your_api_key",
-  authDomain: "your_project_id.firebaseapp.com",
-  projectId: "your_project_id",
-  storageBucket: "your_project_id.appspot.com",
-  messagingSenderId: "your_sender_id",
-  appId: "your_app_id",
+apiKey: "AIzaSyC_VVgyr51C9_BOZdzDegErssQ14pX8W9g",
+  authDomain: "epilogue-8f9f3.firebaseapp.com",
+  projectId: "epilogue-8f9f3",
+  storageBucket: "epilogue-8f9f3.firebasestorage.app",
+  messagingSenderId: "520826081542",
+  appId: "1:520826081542:web:4cd33dd3ee1782aacbce87",
+  measurementId: "G-VV42E3B635"
 };
 
 const app = initializeApp(firebaseConfig);
